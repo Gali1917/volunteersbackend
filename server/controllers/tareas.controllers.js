4d93b4a0cc3d18170aba1795612be727bdfb3cde
Gali1917 <carlgale93@hotmail.com>
2023-03-25 07:27:26 -0500

    Add/Gali1917/Manejo de errores

--- server/controllers/tareas.controllers.js
+++ server/controllers/tareas.controllers.js
@@ -2,31 +2,50 @@ import { trusted } from "mongoose";
 import Tareas from "../models/Tareas.js";
 
 export const getTareas = async (req, res) => {
-     const tareas = await Tareas.find();
-    res.send(tareas);
+    try {
+        const tareas = await Tareas.find();
+        res.send(tareas);
+    } catch (error) {
+        return res.sendStatus(500).json({ message: error.message });
+    }
 }
 
 export const postTareas = async (req, res) => {
-    const {title, description} = req.body;
-    const newTareas = new Tareas({title, description});
-    await newTareas.save();
-    return res.json(newTareas);
+    try {
+        const { title, description } = req.body;
+        const newTareas = new Tareas({ title, description });
+        await newTareas.save();
+        return res.json(newTareas);
+    } catch (error) {
+        return res.sendStatus(500).json({ message: error.message });
+    }
 };
 
 export const putTareas = async (req, res) => {
-    const tareas = await Tareas.findByIdAndUpdate(req.params.id, req.body, {new: true});
-    console.log(tareas);
-    return res.send('recibido');
+    try {
+        const updateTarea = await Tareas.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.send(updateTarea);
+    } catch (error) {
+        return res.sendStatus(500).json({ message: error.message });
+    }
 };
 
 export const deleteTareas = async (req, res) => {
-    const tareaEliminada = await Tareas.findByIdAndDelete(req.params.id);
-    if(!tareaEliminada) return res.sendStatus(404);
-    return res.sendStatus(204);
+    try {
+        const tareaEliminada = await Tareas.findByIdAndDelete(req.params.id);
+        if (!tareaEliminada) return res.sendStatus(404);
+        return res.sendStatus(204);
+    } catch (error) {
+        return res.sendStatus(500).json({ message: error.message });
+    }
 };
 
 export const getTareasId = async (req, res) => {
-    const tareaId = await Tareas.findById(req.params.id);
-    if(!tareaId) return res.sendStatus(404);
-    return res.json(tareaId);
+    try {
+        const tareaId = await Tareas.findById(req.params.id);
+        if (!tareaId) return res.sendStatus(404);
+        return res.json(tareaId);
+    } catch (error) {
+        return res.sendStatus(500).json({ message: error.message });
+    }
 };
