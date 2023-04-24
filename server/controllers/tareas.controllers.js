3466a234d546bd07896f3d6f7d2514dbf4b089a6
Gali1917 <carlgale93@hotmail.com>
2023-04-23 19:28:46 -0500

    Change/Gali1917/Actualizar con imagenes

--- server/controllers/tareas.controllers.js
+++ server/controllers/tareas.controllers.js
@@ -35,12 +35,44 @@ export const postTareas = async (req, res) => {
 };
 
 export const putTareas = async (req, res) => {
+    // try {
+    //     const updateTarea = await Tareas.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    //     return res.send(updateTarea);
+    // } catch (error) {
+    //     return res.status(500).json({ message: error.message });
+    // }
     try {
-        const updateTarea = await Tareas.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        return res.send(updateTarea);
-    } catch (error) {
+        // Buscar la tarea por ID
+        const tarea = await Tareas.findById(req.params.id);
+    
+        // Si no se encuentra la tarea, enviar un error 404
+        if (!tarea) {
+          return res.status(404).json({ message: 'Tarea no encontrada' });
+        }
+    
+        // Actualizar los campos de la tarea
+        tarea.title = req.body.title || tarea.title;
+        tarea.description = req.body.description || tarea.description;
+    
+        // Si el campo de imagen está presente, subir la nueva imagen y eliminar la anterior
+        if (req.files?.image) {
+          const result = await uploadImage(req.files.image.tempFilePath);
+          await fs.remove(tarea.image.public_id); // Eliminar la imagen antigua
+          tarea.image = {
+            url: result.secure_url,
+            public_id: result.public_id
+          };
+          await fs.remove(req.files.image.tempFilePath); // Eliminar el archivo temporal
+        }
+    
+        // Guardar la tarea actualizada en la base de datos
+        const updatedTarea = await tarea.save();
+    
+        // Responder con la tarea actualizada
+        return res.json(updatedTarea);
+      } catch (error) {
         return res.status(500).json({ message: error.message });
-    }
+      }
 };
 
 export const deleteTareas = async (req, res) => {
